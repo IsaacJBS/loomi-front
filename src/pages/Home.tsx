e773552a118c93be3965cd0fdf2d9aba35b3ab5c
Isaac <isaac.jordaobs@gmail.com>
2022-06-17 14:56:57 -0300

    feat: sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 
 // type Props = {}
 
 function Home() {
   return (
-    <Header
-      name="Eduardo"
-      imageUrl="https://mir-s3-cdn-cf.behance.net/projects/404/d4ba0348411353.Y3JvcCw3MDYsNTUyLDI3LDA.png"
-    />
+    <>
+      <Header
+        name="Eduardo"
+        imageUrl="https://mir-s3-cdn-cf.behance.net/projects/404/d4ba0348411353.Y3JvcCw3MDYsNTUyLDI3LDA.png"
+      />
+      <SideBar />
+    </>
   );
 }
 
