849002460ad9d4652104242b3a01acfc89e1784c
Isaac <isaac.jordaobs@gmail.com>
2022-06-17 10:16:43 -0300

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { ChakraProvider, Box, Text, Grid, theme } from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <Text>Olá, Loomi</Text>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-);
+export default function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <Text>Olá, Loomi</Text>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
