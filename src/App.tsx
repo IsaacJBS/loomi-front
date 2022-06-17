6ec370a30910ba109b69ed5fd393a2fee74f8925
Isaac <isaac.jordaobs@gmail.com>
2022-06-17 13:51:10 -0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import Header from "./components/Header";
 
 export default function App() {
-  return <Login />;
+  return (
+    <Header
+      name="Eduardo"
+      imageUrl="https://mir-s3-cdn-cf.behance.net/projects/404/d4ba0348411353.Y3JvcCw3MDYsNTUyLDI3LDA.png"
+    />
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Flex, Text, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
+import "@fontsource/ubuntu";
 
-// type Props = {}
+interface IProps {
+  name: string;
+  imageUrl: string;
+}
 
-function Header() {
+function Header({ name, imageUrl }: IProps) {
   return (
-    <Flex>
-      <Image src={logo} />
-      <Text>Eduardo</Text>
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      p={5}
+      boxShadow="md"
+    >
+      <Image w={50} src={logo} />
+      <Flex alignItems="center" gap={5}>
+        <Text fontFamily="body" textColor="#4E5D66" fontWeight="semibold">
+          {name}
+        </Text>
+        <Image w={10} h={10} borderRadius="50%" src={imageUrl} />
+      </Flex>
     </Flex>
   );
 }
