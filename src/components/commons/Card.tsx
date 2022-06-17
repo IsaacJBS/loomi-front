758b9d30cce27a5526c010d1e71b9976749e3bea
Isaac <isaac.jordaobs@gmail.com>
2022-06-17 15:46:19 -0300

    feat: tag component

--- src/components/commons/Card.tsx
+++ src/components/commons/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Flex, Box, Text } from "@chakra-ui/react";
+import Tag from "./Tag";
 
 function Card() {
   return (
@@ -8,12 +9,14 @@ function Card() {
         <Text fontFamily="body" fontWeight="bold">
           Ticket médio últimas 24h{" "}
         </Text>
-        <Text mt={2} fontSize="12px" fontFamily="body">
-          + 15%{" "}
+        <Tag color="#109E8E" text="+ 15%" />
+        <Text color="#109E8E" mt={2}>
+          em relação a ontem
         </Text>
-        <Text mt={2}>em relação a ontem</Text>
         <Flex mt={3} gap={3}>
-          <Text>R$</Text>
+          <Text fontFamily="body" fontWeight="semi-bold">
+            R$
+          </Text>
           <Text fontFamily="body" fontWeight="bold">
             9.292,00
           </Text>
