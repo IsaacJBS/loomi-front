0d2fcdaaf5cee65a2486d7a2be3012ef9e215ea8
Isaac <isaac.jordaobs@gmail.com>
2022-06-17 13:54:56 -0300

    feat: add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
+import Home from "./pages/Home";
 
 // import Login from "./pages/Login";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 export default function App() {
-  return (
-    <Header
-      name="Eduardo"
-      imageUrl="https://mir-s3-cdn-cf.behance.net/projects/404/d4ba0348411353.Y3JvcCw3MDYsNTUyLDI3LDA.png"
-    />
-  );
+  return <Home />;
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+// type Props = {}
+
+function Home() {
+  return (
+    <Header
+      name="Eduardo"
+      imageUrl="https://mir-s3-cdn-cf.behance.net/projects/404/d4ba0348411353.Y3JvcCw3MDYsNTUyLDI3LDA.png"
+    />
+  );
+}
+
+export default Home;
