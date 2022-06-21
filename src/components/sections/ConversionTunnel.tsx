1de58dbe7966109277af71ec0df1f7376e6df66f
Isaac <isaac.jordaobs@gmail.com>
2022-06-21 12:22:19 -0300

    feat: component CardProductsView

--- src/components/sections/ConversionTunnel.tsx
+++ src/components/sections/ConversionTunnel.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Carousel from "../Carousel";
 // import axios from "../../api/axios";
 import CardSessions from "../cards/CardSessions";
+import CardProductsView from "../cards/CardProductsView";
 
 function ConversionTunnel() {
   // const [cards, setCards] = useState();
   return (
     <Carousel name="Funil de conversão">
       <CardSessions />
+      <CardProductsView />
     </Carousel>
   );
 }
